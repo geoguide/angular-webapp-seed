7ce8a6b2289e791c154b7e9f83be15fc29228ce7
Geoff Guidetti <geoffguidetti@gmail.com>
2015-10-15 09:33:44 -0700

    many performance improvements, bug fixes for changing dashboard.

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -7,7 +7,7 @@
  * # DashboardCtrl
  * Controller of the modioAdminPortal
  */
-angular.module('modioAdminPortal').controller('DashboardCtrl', function (dashboardFactory,$log) {
+angular.module('modioAdminPortal').controller('DashboardCtrl', function (dashboardFactory,$log,applicationFactory) {
 
 	var _this = this;
 	this.loading = true;
@@ -17,12 +17,32 @@ angular.module('modioAdminPortal').controller('DashboardCtrl', function (dashboa
 		templateUrl: 'notes-template.html',
 		title: 'Doctor Notes'
 	};
+//	_this.twoWayMatches = applicationFactory.twoWayMatches;
+	
+	this.get2Way = function(){
+		_this.loading = true;
+		dashboardFactory.get2Way().then(function(result){
+			_this.twoWayMatches = result;
+			dashboardFactory.twoWayMatches = result;
+//			applicationFactory.twoWayMatches = result;
+			_this.loading = false;
+		},function(error){
+			_this.loading = false;
+			$log.error(error);
+		});
+	};
 	
 	this.setTooltip = function(item){
 		_this.tooltipSales = item.sales_notes;
 		_this.tooltipJob = item.modio_notes;
 	};
+	this.twoWayMatches = dashboardFactory.twoWayMatches;
+
 	
-	
+	if(_this.twoWayMatches.length < 1){
+		_this.get2Way();
+	} else {
+		_this.loading = false;
+	}
 	
 });
