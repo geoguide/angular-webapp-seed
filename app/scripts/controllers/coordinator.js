6fed11519c16cdd5ac3b2c95d445ed0a691ee653
Geoff Guidetti <geoffguidetti@gmail.com>
2015-11-12 16:58:11 -0800

    fixes missing url for memberships

--- app/scripts/controllers/coordinator.js
+++ app/scripts/controllers/coordinator.js
@@ -66,8 +66,10 @@ angular.module('modioAdminPortal').controller('CoordinatorCtrl', function ($wind
 			return doctorFactory.getMemberships(coordId);
 		}).then(function(result){
 			for(var fm=0;fm<result.length;fm++){
-				var url = 'https://s3.amazonaws.com/' + S3_URL + '/1/' + result[fm].file_url.substr(0, 8) + '-' + result[fm].file_url.substr(8, 4) + '-' + result[fm].file_url.substr(12, 4) + '-' + result[fm].file_url.substr(16, 4) + '-' + result[fm].file_url.substr(20, 12) + '/' + result[fm].file_url.substr(32);
-				result[fm].profileUrl = url;
+				if(result[fm].file_url){
+					var url = 'https://s3.amazonaws.com/' + S3_URL + '/1/' + result[fm].file_url.substr(0, 8) + '-' + result[fm].file_url.substr(8, 4) + '-' + result[fm].file_url.substr(12, 4) + '-' + result[fm].file_url.substr(16, 4) + '-' + result[fm].file_url.substr(20, 12) + '/' + result[fm].file_url.substr(32);
+					result[fm].profileUrl = url;
+				}	
 			}
 			
 			_this.memberships = result;
