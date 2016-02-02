49108c9ef8f5a6d83501e062db63475a233fc764
vdemchuk <vasyl.demchuk@gmail.com>
2016-02-02 13:54:08 +0200

    DOCTORS-1271 - Add New Rate Type "Daily Rate - Insurance Only"

--- app/scripts/controllers/applicationcontroller.js
+++ app/scripts/controllers/applicationcontroller.js
@@ -125,6 +125,7 @@ angular.module('modioAdminPortal').controller('ApplicationCtrl', function ($scop
 		{ id:0, 'label': 'Daily Weekday (Up to 24hrs)' },
 		{ id:1, 'label': 'Daily Weekday Plus Call (24hrs)' },
 		{ id:2, 'label': 'Daily Weekend (24hrs)' },
+		{ id:7, 'label': 'Daily Rate - Insurance Only' },
 		{ id:3, 'label': 'Hourly' },
 		{ id:4, 'label': 'Overtime' },
 		{ id:5, 'label': 'Holiday Premium' },
