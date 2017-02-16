eba74df8948a9ba04742d658aef7c5532e62b9c4
Ivan Melnychuk <ioann.melnychuk@gmail.com>
2017-02-16 11:58:53 +0200

    DOCTORS-2342 - Admin Provider Notes and Attachments

--- app/scripts/controllers/facility-notes.js
+++ app/scripts/controllers/facility-notes.js
@@ -8,119 +8,132 @@
  * Controller of the modioAdminPortal
  */
 angular.module('modioAdminPortal')
-	.controller('FacilityNotesCtrl', function ($routeParams, facilityFactory, toasty, $log, $modal, $window, s3factory, MODIOCORE) {
-		var _this = this;
-		this.facilityId = $routeParams.id;
-		this.componentId = 3;
-		this.facilityData = null;
-		this.files = [];
-		this.tab = 'facility-notes';
-		this.error = false;
-		this.loading = true;
-		this.membership = false;
-		this.MODIOCORE = MODIOCORE;
+  .controller('FacilityNotesCtrl', function($routeParams, facilityFactory,
+    toasty, $log, $modal, $window, s3factory, MODIOCORE) {
+    var _this = this;
+    this.facilityId = $routeParams.id;
+    this.componentId = 3;
+    this.facilityData = null;
+    this.files = [];
+    this.tab = 'facility-notes';
+    this.error = false;
+    this.loading = true;
+    this.uploading = false;
+    this.membership = false;
+    this.MODIOCORE = MODIOCORE;
 
-		this.get = function (facilityId) {
-			var facilityData = facilityFactory.getFacility(facilityId);
-			facilityData.then(function (data) {
-				_this.facilityData = data;
-				_this.membership = _this.facilityData.settings & _this.MODIOCORE.facilitySettings.values.membership.id;
-				_this.error = false;
-				_this.loading = false;
-			}, function (error) {
-				_this.error = true;
-				_this.facilityData = null;
-				_this.loading = false;
-			});
-		};
+    this.get = function(facilityId) {
+      var facilityData = facilityFactory.getFacility(facilityId);
+      facilityData.then(function(data) {
+        _this.facilityData = data;
+        _this.membership = _this.facilityData.settings & _this.MODIOCORE
+          .facilitySettings.values.membership.id;
+        _this.error = false;
+        _this.loading = false;
+      }, function(error) {
+        _this.error = true;
+        _this.facilityData = null;
+        _this.loading = false;
+      });
+    };
 
-		this.save = function () {
-			facilityFactory.saveFacility(_this.facilityData).then(function (data) {
-				toasty.success('Facility Saved.');
-				_this.doctorData = data;
-			}, function (error) {
-				toasty.error(error.data);
-			});
-		};
+    this.save = function() {
+      facilityFactory.saveFacility(_this.facilityData).then(function(data) {
+        toasty.success('Facility Saved.');
+        _this.doctorData = data;
+      }, function(error) {
+        toasty.error(error.data);
+      });
+    };
 
-		this.processS3 = function(tag, file, componentId, entityId){
-			s3factory.putObject(file, componentId, entityId, tag, function(data, status, headers, config) {
+    this.processS3 = function(tag, file, componentId, entityId, security) {
+      _this.uploading = true;
+      s3factory.putObject(file, componentId, entityId, tag, security, function(data,
+        status, headers, config) {
+        _this.loadUploads();
+        _this.uploading = false;
+        $log.info('s3 upload done');
+      }, function(data, status, headers, config) {
+        _this.uploading = false;
+        $log.info('s3 upload failed');
+      });
+    };
 
-				_this.loadUploads();
-				$log.info('s3 upload done');
-			},function(data, status, headers, config) {
-				$log.info('s3 upload failed');
-			});
-		};
+    this.upload = function(files) {
+      var tag = 'notes';
+      if (files && files.length) {
+        for (var i = 0; i < files.length; i++) {
+          var file = files[i];
+          var filename = files[i].name;
+          _this.processS3(tag, file, _this.componentId, _this.facilityId, 'a');
+        }
+      }
+    };
 
-		this.upload = function (files) {
-			var tag = 'notes';
-			if (files && files.length) {
-				for (var i = 0; i < files.length; i++) {
-					var file = files[i];
-					var filename = files[i].name;
-					_this.processS3(tag, file, _this.componentId, _this.facilityId);
-				}
-			}
-		};
+    this.loadUploads = function() {
+      var key;
+      s3factory.getUploads({
+        componentId: _this.componentId,
+        entityId: _this.facilityId,
+        tag: 'notes'
+      }, function(result) {
+        $log.log(result);
+        _this.files = [];
+        for (var i = 0; i < result.length; i++) {
+          $log.info(result[i].filename);
+          key = result[i].tag;
+          var file = result[i];
+          if (file.filesize) {
+            file.filesizeKB = Math.round(file.filesize / 1024 * 100) /
+              100;
+            file.filesizeMB = Math.round(file.filesize / 1024 / 1024 *
+              100) / 100;
+            if (file.filesizeMB > 1) {
+              file.filesize = file.filesizeMB + ' MB';
+            } else {
+              file.filesize = file.filesizeKB + ' KB';
+            }
+          }
+          _this.files.push(file);
+        }
+        console.log(_this.files);
+        //$scope.$apply();
+      });
+    };
 
-		this.loadUploads = function(){
-			var key;
-			s3factory.getUploads(_this.componentId, _this.facilityId, function(result){
-				$log.log(result);
-				_this.files = [];
-				for(var i = 0; i < result.length;i++){
-					$log.info(result[i].filename);
-					key = result[i].tag;
-					var file = result[i];
-					if (file.filesize) {
-						file.filesizeKB = Math.round(file.filesize / 1024 * 100)/100;
-						file.filesizeMB = Math.round(file.filesize / 1024 / 1024 * 100)/100;
-						if (file.filesizeMB > 1) {
-							file.filesize = file.filesizeMB + ' MB';
-						} else {
-							file.filesize = file.filesizeKB + ' KB';
-						}
-					}
-					_this.files.push(file);
-				}
-				console.log(_this.files);
-				//$scope.$apply();
-			});
-		};
+    this.getFileLink = function(file) {
+      s3factory.getSignedUrl(_this.componentId, file.id).then(function(
+        response) {
+        $log.info(response);
+        $window.location.href = response;
+      });
+    };
 
-		this.getFileLink = function(file){
-			s3factory.getSignedUrl(_this.componentId, file.id).then(function(response){
-				$log.info(response);
-				$window.location.href = response;
-			});
-		};
+    this.deleteUpload = function(file) {
+      $log.log('deleteUpload:' + file.id);
 
-		this.deleteUpload = function(file){
-			$log.log('deleteUpload:' + file.id);
+      s3factory.deleteObject(_this.componentId, _this.facilityId, file.id,
+        function(data, status, headers, config) {
+          //Success
+          $log.log('Deleted file: ' + file.filename);
+          $log.log(data);
+          _this.loadUploads();
+        },
+        function(data, status, headers, config) {
+          //Error
+          $log.log('FAILED to delete file: ' + file.filename);
+          $log.log(data);
+          _this.loadUploads();
+        }
+      );
+    };
 
-			s3factory.deleteObject(_this.componentId, _this.facilityId, file.id,
-				function(data, status, headers, config) {
-					//Success
-					$log.log('Deleted file: ' + file.filename);
-					$log.log(data);
-					_this.loadUploads();
-				},
-				function(data, status, headers, config) {
-					//Error
-					$log.log('FAILED to delete file: ' + file.filename);
-					$log.log(data);
-					_this.loadUploads();
-				}
-			);
-		};
+    /* Init */
 
-		/* Init */
+    var init = function() {
+      _this.get(_this.facilityId);
+      _this.loadUploads();
+    };
 
-		var init = function () {
-			_this.get(_this.facilityId);
-			_this.loadUploads();
-		};
-
-		init();
-	});
+    init();
+  });
--- app/scripts/controllers/provider-notes.js
+++ app/scripts/controllers/provider-notes.js
@@ -9,17 +9,21 @@
  */
 
 angular.module('modioAdminPortal')
-  .controller('ProviderNotesCtrl', function($routeParams, $log, doctorFactory, toasty) {
+  .controller('ProviderNotesCtrl', function($routeParams, $log, doctorFactory,
+    toasty, s3factory) {
     var _this = this;
     this.doctorId = $routeParams.id;
     this.tab = 'provider-notes';
-    this.provider = null;
+    this.componentId = 1; //1 - Doctor
+    this.doctorData = null;
     this.error = false;
     this.loading = true;
+    this.uploading = false;
+  
 
     this.load = function(providerId) {
       return doctorFactory.getDoctor(providerId).then(function(response) {
-        _this.provider = response.data;
+        _this.doctorData = response.data;
         _this.loading = false;
       }).catch(function(error) {
         _this.error = true;
@@ -29,12 +33,12 @@ angular.module('modioAdminPortal')
     };
 
     this.save = function() {
-      doctorFactory.saveDoctor(_this.provider).then(function(data) {
+      doctorFactory.saveDoctor(_this.doctorData).then(function(data) {
         toasty.success({
           title: 'Success!',
           msg: 'Doctor Saved.'
         });
-        _this.provider = data;
+        _this.doctorData = data;
       }, function(error) {
         $log.error(error);
         toasty.error({
@@ -44,5 +48,88 @@ angular.module('modioAdminPortal')
       });
     };
 
+    this.processS3 = function(tag, file, componentId, entityId, security) {
+      this.uploading = true;
+      s3factory.putObject(file, componentId, entityId, tag, security, function(data,
+        status, headers, config) {
+        _this.loadUploads();
+        _this.uploading = false;
+        $log.info('s3 upload done');
+      }, function(data, status, headers, config) {
+        _this.uploading = false;
+        $log.info('s3 upload failed');
+      });
+    };
+
+    this.upload = function(files) {
+      var tag = 'notes';
+      if (files && files.length) {
+        for (var i = 0; i < files.length; i++) {
+          var file = files[i];
+          var filename = files[i].name;
+          _this.processS3(tag, file, _this.componentId, _this.doctorId, 'a');
+        }
+      }
+    };
+
+    this.loadUploads = function() {
+      var key;
+      s3factory.getUploads({
+        componentId: _this.componentId,
+        entityId: _this.doctorId,
+        tag: 'notes'
+      }, function(result) {
+        $log.log(result);
+        _this.files = [];
+        for (var i = 0; i < result.length; i++) {
+          $log.info(result[i].filename);
+          key = result[i].tag;
+          var file = result[i];
+          if (file.filesize) {
+            file.filesizeKB = Math.round(file.filesize / 1024 * 100) /
+              100;
+            file.filesizeMB = Math.round(file.filesize / 1024 / 1024 *
+              100) / 100;
+            if (file.filesizeMB > 1) {
+              file.filesize = file.filesizeMB + ' MB';
+            } else {
+              file.filesize = file.filesizeKB + ' KB';
+            }
+          }
+          _this.files.push(file);
+        }
+        console.log(_this.files);
+        //$scope.$apply();
+      });
+    };
+
+    this.getFileLink = function(file) {
+      s3factory.getSignedUrl(_this.componentId, file.id).then(function(
+        response) {
+        $log.info(response);
+        $window.location.href = response;
+      });
+    };
+
+    this.deleteUpload = function(file) {
+      $log.log('deleteUpload:' + file.id);
+
+      s3factory.deleteObject(_this.componentId, _this.doctorId, file.id,
+        function(data, status, headers, config) {
+          //Success
+          $log.log('Deleted file: ' + file.filename);
+          $log.log(data);
+          _this.loadUploads();
+        },
+        function(data, status, headers, config) {
+          //Error
+          $log.log('FAILED to delete file: ' + file.filename);
+          $log.log(data);
+          _this.loadUploads();
+        }
+      );
+    };
+
     this.load(this.doctorId);
+    this.loadUploads();
   });
