915475c5e7e585aabbe8111e5cf88d767247fb22
Ivan Melnychuk <ioann.melnychuk@gmail.com>
2017-10-05 19:03:17 +0300

    DOCTORS-3030 - Permission Payors by Facility in Admin

--- app/scripts/filters/filters.js
+++ app/scripts/filters/filters.js
@@ -75,4 +75,33 @@ angular.module('modioAdminPortal').filter('tel', function () {
       });
     }
   };
+}).filter('propsFilter', function () {
+  return function(items, props) {
+    var out = [];
+
+    if (angular.isArray(items)) {
+      items.forEach(function(item) {
+        var itemMatches = false;
+
+        var keys = Object.keys(props);
+        for (var i = 0; i < keys.length; i++) {
+          var prop = keys[i];
+          var text = props[prop] && props[prop].toString().toLowerCase();
+          if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
+            itemMatches = true;
+            break;
+          }
+        }
+
+        if (itemMatches) {
+          out.push(item);
+        }
+      });
+    } else {
+      // Let the output be the input untouched
+      out = items;
+    }
+
+    return out;
+  };
 });
